e7a226d38980762da7054ceaf7a3f601c3df00a2
akgusgh <akgusgh1234@gmial.com>
2025-07-30 19:33:29 +0900

    before trip junju

---
+++ Chapter9/AutoImplementedProperth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    internal class AutoImplementedProperth
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name : {birth.Name}");
+        }
+    }
+}
---
+++ Chapter9/PropertyBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+/*
+namespace Chapter9
+{
+    class BirtdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+
+        public int Age
+        {
+            get { return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year; }
+        }
+    }
+    internal class PropertyBase
+    {
+        static void Main(string[] args)
+        {
+            BirtdayInfo birth = new BirtdayInfo();
+            birth.Name = "표선";
+            birth.Birthday = new DateTime(1999, 6, 28);
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
+*/
